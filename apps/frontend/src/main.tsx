16b52f3e76f14f9cdbc2ae200af8dcee4b916e74
Daizaikun <laiglesias.min@gmail.com>
2023-08-18 13:08:37 -0500

    Config: context and next ui

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { NextUIProvider } from "@nextui-org/react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { Providers } from "./context/index.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Providers>
+      <NextUIProvider>
+        <App />
+      </NextUIProvider>
+    </Providers>
+  </React.StrictMode>
+);
