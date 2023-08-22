f8ec146bc8b76837f01c40b7415d3165ebaf0c82
MontoyitaDev <simon1018226487@gmail.com>
2023-08-21 20:43:16 -0500

    create: Creacion de rutas

---
+++ apps/frontend/src/routers/auth/register/Register.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react'
+
+export interface PropsRegister {
+
+}
+
+const Register: FC<PropsRegister> = () => {
+    return (
+        <div>Register</div>
+    )
+}
+
+export default Register
