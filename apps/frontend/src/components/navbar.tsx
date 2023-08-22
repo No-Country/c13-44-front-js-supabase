540811888d8501ccfcdca1ddc484758d2a4c9f6a
matias torres <matias.torres1812@gmail.com>
2023-08-22 15:38:03 -0400

    add: se agrega iconos de arbol y de avion de papel

--- apps/frontend/src/components/navbar.tsx
+++ apps/frontend/src/components/navbar.tsx
@@ -1,4 +1,5 @@
 import { Navbar, NavbarContent, NavbarItem, Link, Button } from "@nextui-org/react";
+import { IconChristmasTree, IconSend } from '@tabler/icons-react';
 import logo from '../img/icono00.png'
 import './navbar.css';
 
@@ -27,9 +28,11 @@ export default function NavbarOffLogin() {
             </NavbarContent>
             <NavbarContent justify="end">
                 <NavbarItem className="hidden lg:flex">
+                    <IconChristmasTree className="mt-3.5 w-7 h-7"/>
                     <Button className="btn1" color="danger" variant="bordered" startContent=''>
                         Iniciar Sesión
                     </Button>
+                    <IconSend className="mt-4 w-6 h-6" />
                     <Button className="btn1" color="danger" variant="bordered" startContent=''>
                         Registrarse
                     </Button>
