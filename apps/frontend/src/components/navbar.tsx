7c48905dc664b5a90689917f1b1d457e6a4cf75b
MontoyitaDev <simon1018226487@gmail.com>
2023-08-22 12:59:31 -0500

    change: Cambiando la ruta del navbar al app.tsx

--- apps/frontend/src/components/navbar.tsx
+++ apps/frontend/src/components/navbar.tsx
@@ -1,12 +1,18 @@
 import { Navbar, NavbarContent, NavbarItem, Link, Button } from "@nextui-org/react";
 import logo from '../img/icono00.png'
 import './navbar.css';
+import { useLocation } from "wouter";
 
 export default function NavbarOffLogin() {
+    const [_location, setLocation] = useLocation(); //_ para especificar que no se utiliza
+
+
     return (
         <Navbar className="nav" shouldHideOnScroll>
-            <img style={{ width: '5rem' }} src={logo} alt="" />
-            <NavbarContent className="hidden sm:flex gap-4" justify="center">
+            <div className="img_nav">
+                <img src={logo} alt="" />
+            </div>
+            <NavbarContent className="options hidden sm:flex gap-4" justify="center">
                 <NavbarItem>
                     <Link color="foreground" href="/destination">
                         Destinos
@@ -25,15 +31,11 @@ export default function NavbarOffLogin() {
             </NavbarContent>
             <NavbarContent justify="end">
                 <NavbarItem className="hidden lg:flex">
-                    <Button className="btn" color="danger" variant="bordered" startContent=''>
-                        <Link href="/login">
-                            Iniciar Sesión
-                        </Link>
+                    <Button onClick={() => setLocation("/login")} className="btn1" color="danger" variant="bordered" startContent=''>
+                        Iniciar Sesión
                     </Button>
-                    <Button className="btn" color="danger" variant="bordered" startContent=''>
-                        <Link href="/register">
-                            Registrarse
-                        </Link>
+                    <Button onClick={() => setLocation("/register")} className="btn1" color="danger" variant="bordered" startContent=''>
+                        Registrarse
                     </Button>
                 </NavbarItem>
             </NavbarContent>
