9db59075f999a02a126bda76b8575611745579b4
Leonardo Iglesias <leo-im@hotmail.com>
2023-08-23 22:08:48 -0500

    add: se agrego el color primario del rosa

--- apps/frontend/tailwind.config.js
+++ apps/frontend/tailwind.config.js
@@ -8,7 +8,11 @@ module.exports = {
 		"./src/**/*.{js,ts,jsx,tsx}",
 	],
 	theme: {
-		extend: {},
+		extend: {
+      colors:{
+        primary: '#D41790',
+      }
+    },
 	},
 	darkMode: "class",
 	plugins: [
