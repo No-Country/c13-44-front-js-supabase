5360653f74826720d95188be1c2416b1da481fda
MontoyitaDev <simon1018226487@gmail.com>
2023-08-26 11:52:23 -0500

    change: Barrels en carpetas y subcarpetas para un mejor orden.

---
+++ apps/frontend/src/components/index.ts
@@ -0,0 +1,8 @@
+export { default as HeadContact } from './Contact/HeadContact';
+export { default as Loading } from './Loading';
+export { default as MapView } from './MapView';
+export { default as Icons } from './MenuIcon/Icons';
+export * from './filters';
+export * from './index';
+export { default as navbar } from './navbar';
+export * from './theme_button';
