4af3a61ee4838235e94f087763b1cf16d30946a3
MontoyitaDev <simon1018226487@gmail.com>
2023-08-26 11:50:43 -0500

    change: Situando el token de acceso para conectar con mapbox

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -4,8 +4,11 @@ import Hotels from "./routers/auth/pages/Hotels";
 import Contact from "./routers/auth/pages/Contact";
 import Login from "./routers/auth/login/Login";
 import Register from "./routers/auth/register/Register";
+import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 import { Route } from "wouter";
 import { Home } from "./routers/home";
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiYmFzdGFyZG9zc3MiLCJhIjoiY2xscjFiZXI2MGJ1bDNjdWd3djVxbnl5eiJ9.SsXPnXm1Na-BkCSHLmNKMQ';
 
 function App() {
   return (
