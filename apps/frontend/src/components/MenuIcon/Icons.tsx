81b6758bc2c5b79753d7471f45acb4c8b3577fcb
MontoyitaDev <simon1018226487@gmail.com>
2023-08-28 12:58:50 -0500

    fix: Arregle los iconos para que quedaran responsives

--- apps/frontend/src/components/MenuIcon/Icons.tsx
+++ apps/frontend/src/components/MenuIcon/Icons.tsx
@@ -4,25 +4,23 @@ import { IconDeviceMobile, IconMail, IconMapPin } from "@tabler/icons-react"
 const Icons: React.FC = () => {
     return (
         <>
-            <div className="flex justify-end pb-9 ">
-                <div className="">
-                    <ul className="flex flex-row mt-12 gap-x-28 font-mono">
-                        <li className="grid justify-items-center">
-                            <IconMapPin className="text-primary" />
-                            <h1 className="">Colombia, Santa Marta, Mamatoco </h1>
-                        </li>
-                        <li className="grid justify-items-center">
-                            <IconDeviceMobile className="text-primary" />
-                            <h1>(+57) 3117652898</h1>
-
-                        </li>
-                        <li className="grid justify-items-center">
-                            <IconMail className="text-primary" />
-                            <h1>leonardoiglesias@teletubie.com</h1>
-                        </li>
-                    </ul>
-                </div>
+            <div className="flex flex-col justify-center md:flex-row pb-9 ">
+                <ul className="flex mt-12 gap-x-28 font-mono flex-col md:flex-row">
+                    <li className="grid justify-items-center md:pt-0">
+                        <IconMapPin className="text-primary" />
+                        <h1 className="">Colombia, Santa Marta, Mamatoco </h1>
+                    </li>
+                    <li className="grid justify-items-center pt-4 md:pt-0">
+                        <IconDeviceMobile className="text-primary" />
+                        <h1>(+57) 3117652898</h1>
+                    </li>
+                    <li className="grid justify-items-center pt-4 md:pt-0">
+                        <IconMail className="text-primary" />
+                        <h1>leonardoiglesias@teletubie.com</h1>
+                    </li>
+                </ul>
             </div>
+
 
         </>
     )
