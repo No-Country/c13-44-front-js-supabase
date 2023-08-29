da561c91fde3a6d216bd8a3042bdf38eb94f5e25
MontoyitaDev <simon1018226487@gmail.com>
2023-08-29 12:43:40 -0500

    fix: Arreglando la side navbar para que sea responsive y delete para la ruta hoteles

--- apps/frontend/src/components/navbar.tsx
+++ apps/frontend/src/components/navbar.tsx
@@ -1,47 +1,100 @@
-import { Navbar, NavbarContent, NavbarItem, Link, Button } from "@nextui-org/react";
-import { IconChristmasTree, IconSend } from '@tabler/icons-react';
-import { ThemeSwitcher } from './theme_button'
-import logo from '../img/icono00.png'
-import './navbar.css';
-
-export default function NavbarOffLogin() {
+import {
+    Navbar,
+    NavbarContent,
+    NavbarItem,
+    Link,
+    Button,
+    NavbarMenu,
+    NavbarMenuItem,
+    NavbarMenuToggle,
+    NavbarBrand,
+  } from "@nextui-org/react";
+  import { IconChristmasTree, IconSend } from "@tabler/icons-react";
+  import logo from "../img/icono00.png";
+  import "./navbar.css";
+  import { useLocation } from "wouter";
+  import React from "react";
+  
+  
+  export default function NavbarOffLogin() {
+    const [, setLocation] = useLocation(); //_ para especificar que no se utiliza
+    const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+  
+    const menuItems = [
+      {name: "Destinos", route: '/destination'},
+      {name: "Contacto", route: '/contact'},
+      {name: "Iniciar Sesión", route: '/login'},
+      {name: "Registro", route: '/register'},
+    ];
+  
     return (
-        <Navbar className="nav" shouldHideOnScroll>
-            <div className="img_nav">
-                <img  src={logo} alt="" />
+      <Navbar onMenuOpenChange={setIsMenuOpen} className="nav" shouldHideOnScroll>
+        <NavbarContent>
+          <NavbarMenuToggle
+            aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+            className="sm:hidden"
+          />
+          <NavbarBrand>
+            <div className="img_nav" onClick={() => setLocation('/')}>
+              <img src={logo} alt="" />
             </div>
-            <NavbarContent className="options hidden sm:flex gap-4" justify="center">
-                <NavbarItem>
-                    <Link color="foreground" href="/destination">
-                        Destinos
-                    </Link>
-                </NavbarItem>
-                <NavbarItem >
-                    <Link href="/hotels" color="foreground">
-                        Hoteles
-                    </Link>
-                </NavbarItem>
-                <NavbarItem>
-                    <Link color="foreground" href="/contact">
-                        Contacto
-                    </Link>
-                </NavbarItem>
-                <NavbarItem>
-                    <ThemeSwitcher />
-                </NavbarItem>
-            </NavbarContent>
-            <NavbarContent justify="end">
-                <NavbarItem className="hidden lg:flex">
-                    <IconChristmasTree className="mt-3.5 w-7 h-7"/>
-                    <Button className="btn1" variant="bordered" startContent=''>
-                        Iniciar Sesión
-                    </Button>
-                    <IconSend className="mt-4 w-6 h-6" />
-                    <Button className="btn1" variant="bordered" color="default" startContent=''>
-                        Registrarse
-                    </Button>
-                </NavbarItem>
-            </NavbarContent>
-        </Navbar>
+          </NavbarBrand>
+        </NavbarContent>
+        <NavbarContent className="options hidden sm:flex gap-4" justify="center">
+          <NavbarItem>
+            <Link color="foreground" href="/destination">
+              Destinos
+            </Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Link color="foreground" href="/contact">
+              Contacto
+            </Link>
+          </NavbarItem>
+        </NavbarContent>
+        <NavbarContent justify="end">
+          <NavbarItem className="hidden lg:flex">
+            <IconChristmasTree className="mt-3.5 w-7 h-7" />
+            <Button
+              onClick={() => setLocation("/login")}
+              className="btn1"
+              color="danger"
+              variant="bordered"
+              startContent=""
+            >
+              Iniciar Sesión
+            </Button>
+            <IconSend className="mt-4 w-6 h-6" />
+            <Button
+              onClick={() => setLocation("/register")}
+              className="btn1"
+              color="danger"
+              variant="bordered"
+              startContent=""
+            >
+              Registrarse
+            </Button>
+          </NavbarItem>
+        </NavbarContent>
+        <NavbarMenu>
+          {menuItems.map((item, index) => (
+            <NavbarMenuItem key={`${item}-${index}`}>
+              <Link
+                color={
+                    index > 1 ? 'primary' : 'foreground'
+                }
+                className="w-full"
+                onClick={() => {
+                  setLocation(item.route)
+                }}
+                size="lg"
+              >
+                {item.name}
+              </Link>
+            </NavbarMenuItem>
+          ))}
+        </NavbarMenu>
+      </Navbar>
     );
-}
+  }
+  
