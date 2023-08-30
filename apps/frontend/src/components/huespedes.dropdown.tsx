951a60f0941ddec60119446bed9983df55ee0684
Leonardo Iglesias <leo-im@hotmail.com>
2023-08-29 22:24:06 -0500

    update: componente huspedes - se mejoro el estado de huspedes

--- apps/frontend/src/components/huespedes.dropdown.tsx
+++ apps/frontend/src/components/huespedes.dropdown.tsx
@@ -1,134 +1,102 @@
-import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Button } from "@nextui-org/react";
+import {
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownItem,
+  Button,
+} from "@nextui-org/react";
 import { useState } from "react";
+
+const HuespedesVarians = [
+  {
+    name: "Adultos",
+    count: 0,
+  },
+  {
+    name: "Niños",
+    count: 0,
+  },
+  {
+    name: "Bebés",
+    count: 0,
+  },
+  {
+    name: "Mascotas",
+    count: 0,
+  },
+];
+
 export default function Huespedes() {
-  const [counters, setCounters] = useState([0, 0, 0, 0]); // Adultos, Niños, Mascotas, Bebés
+  const [HuspedesState, setCounters] = useState(HuespedesVarians); // Adultos, Niños, Mascotas, Bebés
 
   const handleCounterIncrement = (index: number) => {
-    const updatedCounters = [...counters];
-    updatedCounters[index] += 1;
-    setCounters(updatedCounters);
+    const updatedHuspedes = [...HuspedesState];
+    updatedHuspedes[index].count += 1;
+    setCounters(updatedHuspedes);
   };
 
   const handleCounterDecrement = (index: number) => {
-    if (counters[index] > 0) {
-      const updatedCounters = [...counters];
-      updatedCounters[index] -= 1;
-      setCounters(updatedCounters);
-    }
-  };
+    if (HuspedesState[index].count < 0) return;
 
-  const total = counters.reduce((sum, counter) => sum + counter, 0);
+    const updatedHuspedes = [...HuspedesState];
+    updatedHuspedes[index].count -= 1;
+    setCounters(updatedHuspedes);
+  };
 
   return (
     <Dropdown>
       <DropdownTrigger>
-        <Button variant="bordered" className="flex justify-start w-[20rem] h-[3.5rem] text-lg pointer-events-auto">
-          <span>Huespedes: {total}</span>
+        <Button
+          variant="bordered"
+          className="flex justify-start w-[20rem] h-[3.5rem] text-lg pointer-events-auto"
+        >
+          <span>
+            Huespedes:{" "}
+            {HuspedesState.reduce((acc, counter) => acc + counter.count, 0)}
+          </span>
         </Button>
       </DropdownTrigger>
-      <DropdownMenu 
-        aria-label="Action event example" 
+      <DropdownMenu
+        aria-label="Action event example"
         closeOnSelect={false}
-        
-        className="dropdown-menu pointer-events-none">
-        <DropdownItem className="flex flex-row">
-          <span>Adultos</span>
-          <Button
-            className="pointer-events-auto cursor-pointer"
-            onClick={(e) => {
-              e.stopPropagation();
-              handleCounterDecrement(0);
-            }}
-            style={{ margin: "0 4px" }}
-          >
-            -
-          </Button>
-          <span>{counters[0]}</span>
-          <Button
-            className="pointer-events-auto cursor-pointer"
-            onClick={(e) => {
-              e.stopPropagation();
-              handleCounterIncrement(0);
-            }}
-            style={{ margin: "0 4px" }}
-          >
-            +
-          </Button>
-        </DropdownItem>
-        <DropdownItem className="flex flex-row">
-          <span>Niños</span>
-          <Button
-            className="pointer-events-auto cursor-pointer"
-            onClick={(e) => {
-              e.stopPropagation();
-              handleCounterDecrement(1);
-            }}
-            style={{ margin: "0 4px" }}
-          >
-            -
-          </Button>
-          <span>{counters[1]}</span>
-          <Button
-            className="pointer-events-auto cursor-pointer"
-            onClick={(e) => {
-              e.stopPropagation();
-              handleCounterIncrement(1);
-            }}
-            style={{ margin: "0 4px" }}
-          >
-            +
-          </Button>
-        </DropdownItem>
-        <DropdownItem className="flex flex-row">
-          <span>Bebés</span>
-          <Button
-            className="pointer-events-auto cursor-pointer"
-            onClick={(e) => {
-              e.stopPropagation();
-              handleCounterDecrement(2);
-            }}
-            style={{ margin: "0 4px" }}
-          >
-            -
-          </Button>
-          <span>{counters[2]}</span>
-          <Button
-            className="pointer-events-auto cursor-pointer"
-            onClick={(e) => {
-              e.stopPropagation();
-              handleCounterIncrement(2);
-            }}
-            style={{ margin: "0 4px" }}
-          >
-            +
-          </Button>
-        </DropdownItem>
-        <DropdownItem className="flex flex-row">
-          <span>Mascotas</span>
-          <Button
-            className="pointer-events-auto cursor-pointer"
-            onClick={(e) => {
-              e.stopPropagation();
-              handleCounterDecrement(3);
-            }}
-            style={{ margin: "0 4px" }}
-          >
-            -
-          </Button>
-          <span>{counters[3]}</span>
-          <Button
-            className="pointer-events-auto cursor-pointer"
-            onClick={(e) => {
-              e.stopPropagation();
-              handleCounterIncrement(3);
-            }}
-            style={{ margin: "0 4px" }}
-          >
-            +
-          </Button>
-        </DropdownItem>
-        <DropdownItem style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
-        </DropdownItem>
+        className="dropdown-menu pointer-events-none"
+      >
+        {HuespedesVarians.map((Huesped, index) => {
+          return (
+            <DropdownItem className="flex flex-row" key={Huesped.name}>
+              <span>{Huesped.name}</span>
+              <Button
+                className="pointer-events-auto cursor-pointer"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  handleCounterDecrement(index);
+                }}
+                style={{ margin: "0 4px" }}
+              >
+                -
+              </Button>
+              <span>{Huesped.count}</span>
+              <Button
+                className="pointer-events-auto cursor-pointer"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  handleCounterIncrement(index);
+                }}
+                style={{ margin: "0 4px" }}
+              >
+                +
+              </Button>
+            </DropdownItem>
+          );
+        })}
+
+        {/*         <DropdownItem
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        ></DropdownItem> */}
       </DropdownMenu>
     </Dropdown>
   );
