6451b057afaf00bddbbc26fea59177df52563134
MontoyitaDev <129682591+montoyitadevelp@users.noreply.github.com>
2023-08-30 14:27:23 -0500

    Update Login.tsx

--- apps/frontend/src/routers/auth/login/Login.tsx
+++ apps/frontend/src/routers/auth/login/Login.tsx
@@ -36,7 +36,7 @@ const Login = () => {
   const toggleVisibility = () => setIsVisible(!isVisible);
 
   return (
-    <div className="flex flex-row justify-center pt-[10rem]">
+    <div className="flex flex-row justify-center py-[10rem]">
       <section>
         <div className="md:w-96 w-full">
           <form className="space-y-5 " onSubmit={handleSubmit(onSubmit as any)} >
