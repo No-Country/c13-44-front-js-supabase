e71e96c8eedaecc6ede4cfcdd87bef668de0266c
MontoyitaDev <simon1018226487@gmail.com>
2023-08-30 09:35:19 -0500

    add: Añadiendo MouseEvent a los components

--- apps/frontend/src/components/card.destinos.tsx
+++ apps/frontend/src/components/card.destinos.tsx
@@ -1,26 +1,31 @@
-import {Card, CardFooter, Image, Chip} from "@nextui-org/react";
+import { Card, CardFooter, Image, Chip } from "@nextui-org/react";
 import { IconMapPinFilled } from "@tabler/icons-react";
 import destino from '../img/destino.webp'
+import { HtmlHTMLAttributes } from "react";
 
-export default function CardDestinos() {
+type TypeCardDestinos = HtmlHTMLAttributes<HTMLElement> 
+
+export default function CardDestinos({ onClick }: TypeCardDestinos) {
   return (
     <Card
       isFooterBlurred
       radius="lg"
-      className="border-none w-[15rem] h-[15rem]"
+      className="border-none w-[15rem] h-[15rem] hover:cursor-pointer"
+      onClick={onClick}
     >
       <Image
         alt="Woman listing to music"
         className="object-cover max-w-sm"
         src={destino}
+        onClick={onClick}
       />
-      <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
-      <section className="flex flex-row">
-        <div className="text-primary">
+      <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10" onClick={onClick}>
+        <section className="flex flex-row">
+          <div className="text-primary">
             <IconMapPinFilled />
-        </div>
-        <p className="text-tiny text-white/80 mt-1">Colombia</p>
-      </section>
+          </div>
+          <p className="text-tiny text-white/80 mt-1">Colombia</p>
+        </section>
         <Chip className="text-tiny text-white bg-black/20" variant="flat" color="default" radius="lg" size="sm">
           Bogota
         </Chip>
