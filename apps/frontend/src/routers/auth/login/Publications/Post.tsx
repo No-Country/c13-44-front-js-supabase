f47d146a52126985a0372e41f79aef9078f9dfc2
MontoyitaDev <simon1018226487@gmail.com>
2023-08-30 21:13:21 -0500

    add: Creando componente principal que es padre de CardList y TitlePost

---
+++ apps/frontend/src/routers/auth/login/Publications/Post.tsx
@@ -0,0 +1,40 @@
+
+import React, {
+  useState
+} from 'react'
+import {
+  SkeletonCard
+} from './Skeleton'
+import {
+  PropsCard, PropsTitle
+} from '../Interface'
+
+import CardList from './CardList'
+import TitlePost from './TitlePost'
+
+const Post: React.FC = () => {
+  const [postCard, ] = useState<PropsCard[]>([
+    {
+      posting: <SkeletonCard />,
+
+    }
+  ])
+
+  const [postTitle, ] = useState<PropsTitle[]>([
+    {
+      title: <TitlePost />,
+    }
+  ])
+
+
+  return (
+    <>
+      <CardList 
+      postCard={postCard} 
+      postTitle={postTitle} 
+      />
+    </>
+  )
+}
+
+export default Post
