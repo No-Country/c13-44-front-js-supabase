6ea5a240fe79f71267a37fb8637691576924fcd6
MontoyitaDev <simon1018226487@gmail.com>
2023-08-30 21:14:34 -0500

    add: Agregando titulo para ser mapeado en su padre

---
+++ apps/frontend/src/routers/auth/login/Publications/TitlePost.tsx
@@ -0,0 +1,16 @@
+import { Divider } from "@nextui-org/react"
+
+
+const TitlePost = () => {
+    return (
+        <>
+            <div className="space-y-1">
+                <h4 className="text-2xl font-medium">Mis publicaciónes</h4>
+                <p className="text-small text-default-400">"Explora tu historia: Donde tus publicaciones cobran vida"</p>
+            </div>
+            <Divider className="my-[2rem]" />
+        </>
+    )
+}
+
+export default TitlePost
