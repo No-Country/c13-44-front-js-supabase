d0e0cb94bc7d7ff03bc6b9f36ab7f99114e5ae95
MontoyitaDev <simon1018226487@gmail.com>
2023-08-31 11:12:55 -0500

    fix: Mejorando ruta para llegar a postvivienda

--- apps/frontend/src/routers/auth/login/Publications/TitlePost.tsx
+++ apps/frontend/src/routers/auth/login/Publications/TitlePost.tsx
@@ -1,13 +1,22 @@
-import { Divider } from "@nextui-org/react"
+import { Button, Divider } from "@nextui-org/react"
+import { IconPhotoEdit } from "@tabler/icons-react"
+import { useLocation } from "wouter"
+
+
 
 
 const TitlePost = () => {
+    const [, setLocation] = useLocation()
+
     return (
         <>
             <div className="space-y-1">
                 <h4 className="text-2xl font-medium">Mis publicaciónes</h4>
                 <p className="text-small text-default-400">"Explora tu historia: Donde tus publicaciones cobran vida"</p>
             </div>
+            <Button onClick={() => setLocation('/postvivienda')} className="top-3" color="success" endContent={<IconPhotoEdit />}>
+                Subir publicación
+            </Button>
             <Divider className="my-[2rem]" />
         </>
     )
