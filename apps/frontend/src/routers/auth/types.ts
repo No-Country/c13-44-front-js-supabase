e5b323977769acb93f4b7a5a88c29e33f9e84c04
MontoyitaDev <simon1018226487@gmail.com>
2023-09-02 13:54:10 -0500

    add: Types para manejarlos entre el hook de useState y pasarlos a los childrens.

---
+++ apps/frontend/src/routers/auth/types.ts
@@ -0,0 +1,13 @@
+
+//Indicamos que el Token es un objeto para pasarlo al padre y a su children UsuarioPerfil
+export type Token = {
+    user: {
+      user_metadata: {
+        user_name: '',
+      }
+    }
+  }
+  
+ export type PerfilToken = {
+    token: Token;
+  }
