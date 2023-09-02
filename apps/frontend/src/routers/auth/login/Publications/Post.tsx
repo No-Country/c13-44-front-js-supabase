987ce16e8c0a1b5f1532e9de99cfe056bf00507c
MontoyitaDev <simon1018226487@gmail.com>
2023-09-02 14:02:29 -0500

    fix: Integrando el borrador para hacer las funciones de CRUD a futuro

--- apps/frontend/src/routers/auth/login/Publications/Post.tsx
+++ apps/frontend/src/routers/auth/login/Publications/Post.tsx
@@ -6,32 +6,46 @@ import {
   SkeletonCard
 } from './Skeleton'
 import {
+  PropsAction,
   PropsCard, PropsTitle
 } from '../Interface'
 
 import CardList from './CardList'
 import TitlePost from './TitlePost'
 
-const Posting: React.FC = () => {
-  const [postCard, ] = useState<PropsCard[]>([
+import ActionsCard from '../CreatePost/ActionsCard'
+import { PerfilToken } from '../../types'
+
+
+const Posting = ({token}: PerfilToken) => {
+  const [postCard,] = useState<PropsCard[]>([
     {
       posting: <SkeletonCard />,
 
     }
   ])
 
-  const [postTitle, ] = useState<PropsTitle[]>([
+  const [postTitle,] = useState<PropsTitle[]>([
     {
       title: <TitlePost />,
     }
   ])
 
 
+  const [postAction,] = useState<PropsAction[]>([
+    {
+      adding: <ActionsCard />
+    }
+  ])
+
+
   return (
     <>
-      <CardList 
-      postCard={postCard} 
-      postTitle={postTitle} 
+      {token.user.user_metadata.user_name}
+      <CardList
+        postCard={postCard}
+        postTitle={postTitle}
+        postAction={postAction}
       />
     </>
   )
