fd72da52323ca21048586a11d0a6df8d126361b6
montoyitadevelp <simon1018226487@gmail.com>
2023-09-08 10:38:37 -0500

    fix: auth + validation + routes

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import NavbarOffLogin from "./components/NavbarLogout/navbarlogout";
+import NavbarOffLogin from "./components/NavbarLogout/NavbarOffLogin";
 import Footer from "./components/footer";
 import Contact from "./routers/auth/pages/Contact";
 import Destination from "./routers/auth/pages/Destination";
@@ -7,9 +7,8 @@ import Hotels from "./routers/auth/pages/Hotels";
 import mapboxgl from "mapbox-gl"; // or "const mapboxgl = require('mapbox-gl');"
 import Register from "./routers/auth/register/Register";
 
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { Route } from "wouter";
-import NavbarOnLogin from "./components/NavbarLogin/navbarlogin";
 import { AuthContext } from "./context/Auth";
 import Login from "./routers/auth/login/Login";
 import Posting from "./routers/auth/login/Publications/Post";
@@ -20,39 +19,50 @@ import { Post } from "./routers/auth/pages/post";
 import { Home } from "./routers/home";
 
 mapboxgl.accessToken =
-	"pk.eyJ1IjoiYmFzdGFyZG9zc3MiLCJhIjoiY2xscjFiZXI2MGJ1bDNjdWd3djVxbnl5eiJ9.SsXPnXm1Na-BkCSHLmNKMQ";
+  "pk.eyJ1IjoiYmFzdGFyZG9zc3MiLCJhIjoiY2xscjFiZXI2MGJ1bDNjdWd3djVxbnl5eiJ9.SsXPnXm1Na-BkCSHLmNKMQ";
 
 function App() {
-	const { setUser } = AuthContext();
+  const { setUser, user } = AuthContext();
 
-	useEffect(() => {
-		setUser();
-	}, [setUser]);
+  useEffect(() => {
+    setUser();
+  }, [setUser]);
 
-	return (
-		<>
-			<span className="container min-h-screen flex flex-col mx-auto">
-				{/* Si el usuario se loguea entra con avatar y se le eliminan los botones de login y register, por el contrario no */}
-				<NavbarOffLogin />
-				<Route path="/" component={Home} />
-				<Route path="/destination" component={Destination} />
-				<Route path="/hotels" component={Hotels} />
-				<Route path="/contact" component={Contact} />
-				<Route path="/login" component={Login} />
-				<Route path="/register" component={Register} />
-				<Route path="/404" component={Error404} />
+  return (
+    <>
+      <span className="container min-h-screen flex flex-col mx-auto">
+        <NavbarOffLogin />
+        {/* Si existe ingresa a estas rutas */}
+        {user ? (
+          <>
+            <Route path="/" component={Home} />
+            <Route path="/destination" component={Destination} />
+            <Route path="/hotels" component={Hotels} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/perfil" component={UsuarioPerfil} />
+            <Route path="/404" component={Error404} />
+            <Route path="/reservacion" component={Reservar} />
+            <Route path="/postvivienda" component={Post} />
+            <Route path="/posting" component={Posting} />
+          </>
+        ) : (
+          <>
+            <Route path="/" component={Home} />
+            <Route path="/404" component={Error404} />
+            <Route path="/reservacion" component={Reservar} />
+            <Route path="/destination" component={Destination} />
+            <Route path="/hotels" component={Hotels} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+          </>
+        )}
+        {/* Si no existe ingresa a estas */}
 
-				<Route path="/reservacion" component={Reservar} />
-
-				<Route path="/perfil" component={UsuarioPerfil} />
-
-				<Route path="/postvivienda" component={Post} />
-				<Route path="/posting" component={Posting} />
-
-				<Footer />
-			</span>
-		</>
-	);
+        <Footer />
+      </span>
+    </>
+  );
 }
 
 export default App;
