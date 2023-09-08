98ea956595af954c724676498d752be740d02898
montoyitadevelp <simon1018226487@gmail.com>
2023-09-08 10:39:31 -0500

    fix: Delete token.

--- apps/frontend/src/routers/auth/login/Publications/Post.tsx
+++ apps/frontend/src/routers/auth/login/Publications/Post.tsx
@@ -1,42 +1,40 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { PropsAction, PropsCard, PropsTitle } from "../Interface";
 import { SkeletonCard } from "./Skeleton";
 
 import CardList from "./CardList";
 import TitlePost from "./TitlePost";
 
-import { PerfilToken } from "../../types";
 import ActionsCard from "../CreatePost/ActionsCard";
 
 const Posting = () => {
-	const [postCard] = useState<PropsCard[]>([
-		{
-			posting: <SkeletonCard />,
-		},
-	]);
+  const [postCard] = useState<PropsCard[]>([
+    {
+      posting: <SkeletonCard />,
+    },
+  ]);
 
-	const [postTitle] = useState<PropsTitle[]>([
-		{
-			title: <TitlePost />,
-		},
-	]);
+  const [postTitle] = useState<PropsTitle[]>([
+    {
+      title: <TitlePost />,
+    },
+  ]);
 
-	const [postAction] = useState<PropsAction[]>([
-		{
-			adding: <ActionsCard />,
-		},
-	]);
+  const [postAction] = useState<PropsAction[]>([
+    {
+      adding: <ActionsCard />,
+    },
+  ]);
 
-	return (
-		<>
-			{token.user.user_metadata.user_name}
-			<CardList
-				postCard={postCard}
-				postTitle={postTitle}
-				postAction={postAction}
-			/>
-		</>
-	);
+  return (
+    <>
+      <CardList
+        postCard={postCard}
+        postTitle={postTitle}
+        postAction={postAction}
+      />
+    </>
+  );
 };
 
 export default Posting;
