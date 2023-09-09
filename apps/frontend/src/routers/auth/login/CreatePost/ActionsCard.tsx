2dec53e3cc926786bb661e132b812e2d7bb84159
montoyitadevelp <simon1018226487@gmail.com>
2023-09-09 18:09:13 -0500

    fix: Fixing buttons

--- apps/frontend/src/routers/auth/login/CreatePost/ActionsCard.tsx
+++ apps/frontend/src/routers/auth/login/CreatePost/ActionsCard.tsx
@@ -1,21 +1,21 @@
-import { Button } from '@nextui-org/react'
-import { 
-  IconEdit, 
-  IconTrash 
-} from '@tabler/icons-react'
-import React from 'react'
+import { Button } from "@nextui-org/react";
+import { IconEdit, IconTrash } from "@tabler/icons-react";
+import React from "react";
+
 
 const ActionsCard: React.FC = () => {
+
+
   return (
-    <div className='flex space-x-12 pb-[1rem]'>
-      <Button  color="default"  endContent={<IconEdit />}>
+    <div className="flex space-x-12 py-[1rem]">
+      <Button color="default" endContent={<IconEdit />}>
         Editar
       </Button>
-      <Button color="danger"  startContent={<IconTrash />}>
+      <Button color="danger" startContent={<IconTrash />}>
         Borrar
       </Button>
     </div>
-  )
-}
+  );
+};
 
-export default ActionsCard
+export default ActionsCard;
