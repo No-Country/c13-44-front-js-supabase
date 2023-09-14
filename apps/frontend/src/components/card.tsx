236dd23100fd7e5fd75a3b6cffa60e35a08e1094
Leonardo Iglesias <leo-im@hotmail.com>
2023-09-13 20:38:17 -0500

    fix: card with

--- apps/frontend/src/components/card.tsx
+++ apps/frontend/src/components/card.tsx
@@ -10,17 +10,21 @@ type CardPropiedades = HtmlHTMLAttributes<HTMLElement> & {
 	imagen?: string;
 	titulo?: string;
 	descuento?: string | number;
+	autoWith?: boolean;
 };
 
 export default function CardPropiedades({
 	onClick,
+	autoWith,
 	...props
 }: CardPropiedades) {
 	return (
 		<>
 			<Card
 				isFooterBlurred
-				className="w-64 h-[300px] col-span-12 sm:col-span-5 pointer-events-auto"
+				className={`h-[300px]  pointer-events-auto ${
+					!autoWith && "w-64 h-[300px]"
+				}`}
 			>
 				<CardHeader
 					className="absolute z-10 top-1 flex-col items-start"
