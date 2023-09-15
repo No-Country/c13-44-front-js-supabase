2460ca107fd6daf8a23855cf6f9b1f60d6b9c03b
montoyitadevelp <simon1018226487@gmail.com>
2023-09-15 11:42:25 -0500

    fix: fixed imports

--- apps/frontend/src/routers/auth/pages/perfil.tsx
+++ apps/frontend/src/routers/auth/pages/perfil.tsx
@@ -104,7 +104,7 @@ export default function UsuarioPerfil() {
                   );
                 })}
                
-               
+    
                   <MisPublicaciones />
   
               </Tab>
